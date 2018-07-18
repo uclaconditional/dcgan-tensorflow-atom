8c941d09b9d720df6c7c9c132c15abf0b20783a5
Cho Hye Min <pedalioaspare@gmail.com>
2018-07-18 14:22:08 -0700

    Add gen rand images

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import scipy.misc
 import numpy as np
 
 from model import DCGAN
-from utils import pp, visualize, to_json, show_all_variables, generate_random_image
+from utils import pp, visualize, to_json, show_all_variables, generate_random_images
 
 import tensorflow as tf
 
