f5d80b13b706eb0334158bd2842efdd7093f3354
Cho Hye Min <pedalioaspare@gmail.com>
2018-07-20 10:21:59 -0700

    Add generate walk in latent space

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import scipy.misc
 import numpy as np
 
 from model import DCGAN
-from utils import pp, visualize, to_json, show_all_variables, generate_random_images, encode, generate_image_from_seed
+# from utils import pp, visualize, to_json, show_all_variables, generate_random_images, encode, generate_image_from_seed
+import utils
 
 import tensorflow as tf
 
