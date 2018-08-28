7cb756d6f7c8873d6b5e14ed9e4d17604be34df8
Hye Min Cho <3820319+hyeminchocho@users.noreply.github.com>
2018-08-28 14:33:51 -0700

    Add option to model.py

--- model.py
+++ model.py
@@ -18,7 +18,7 @@ class DCGAN(object):
          batch_size=64, sample_num = 64, output_height=64, output_width=64,
          y_dim=None, z_dim=100, gf_dim=64, df_dim=64,
          gfc_dim=1024, dfc_dim=1024, c_dim=3, dataset_name='default',
-         input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None, data_dir='./data'):
+               input_fname_pattern='*.jpg', checkpoint_dir=None, sample_dir=None, data_dir='./data', checkpoint_name=None):
     """
 
     Args:
