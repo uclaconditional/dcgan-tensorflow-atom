627dec14e1f248130bc36aec11828e99b94a7f27
Hye Min Cho <3820319+hyeminchocho@users.noreply.github.com>
2018-09-25 17:02:01 -0700

    Add mode 15 sin cycle all 100

--- utils.py
+++ utils.py
@@ -343,7 +343,7 @@ def generate_sin_cycle_all_100(sess, dcgan, config):
             # Update seed with sin
             seed = orig_seed[:]
             seed_idx = int(int(curr_frame) / int(frames_per_cycle)) % num_total_frames
-            print("seed_idx: " + str(seed_idx) + " : " + str(curr_frame) + " / " + str(frames_per_cycle) " % " + num_total_frames)
+            print("seed_idx: " + str(seed_idx) + " : " + str(curr_frame) + " / " + str(frames_per_cycle) + " % " + num_total_frames)
             seed[seed_idx] = math.sin((curr_frame % frames_per_cycle) * sin_step)
             curr_frame+=1
 
