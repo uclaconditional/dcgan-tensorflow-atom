be5a63bd16103f90284ab1c96796713362a47d77
Hye Min Cho <3820319+hyeminchocho@users.noreply.github.com>
2018-09-27 12:17:27 -0700

    mode 10 not start at 0

--- utils.py
+++ utils.py
@@ -696,9 +696,10 @@ def generate_continuous_interps_from_json(sess, dcgan, config):
 
 def slerp(val, low, high):
     """Code from https://github.com/soumith/dcgan.torch/issues/14"""
-    print("MEEE slepr low: " + str(low.shape) + ", high: " + str(high.shape))
+    # print("MEEE slepr low: " + str(low.shape) + ", high: " + str(high.shape))
     omega = np.arccos(np.clip(np.dot(low/np.linalg.norm(low), high/np.linalg.norm(high)), -1, 1))
     so = np.sin(omega)
+    print("omega: " + omega + " so: " + so)
     if so == 0:
         return (1.0-val) * low + val * high # L'Hopital's rule/LERP
     return np.sin((1.0-val)*omega) / so * low + np.sin(val*omega) / so * high
