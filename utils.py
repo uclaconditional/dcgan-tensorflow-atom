079b32a9b18d27ebae89d223a8b0518353d01631
Hye Min Cho <3820319+hyeminchocho@users.noreply.github.com>
2018-09-27 12:31:40 -0700

    mode 10 not start at 0

--- utils.py
+++ utils.py
@@ -700,7 +700,7 @@ def slerp(val, low, high):
     omega = np.arccos(np.clip(np.dot(low/np.linalg.norm(low), high/np.linalg.norm(high)), -1, 1))
     so = np.sin(omega)
     print("omega: " + str(omega) + " so: " + str(so))
-    print("typeomega: " + type(omega) + " typeso: " + sype(so))
+    print("typeomega: " + str(type(omega)) + " typeso: " + str(type(so)))
     if so == 0:
         return (1.0-val) * low + val * high # L'Hopital's rule/LERP
     return np.sin((1.0-val)*omega) / so * low + np.sin(val*omega) / so * high
