2c598a18eebdf659621abe8fe097243b1e54b15b
Hye Min Cho <3820319+hyeminchocho@users.noreply.github.com>
2018-12-11 17:37:57 -0800

    Add mode 16 command line var

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ flags.DEFINE_integer("generate_test_images", 100, "Number of images to generate 
 flags.DEFINE_string("input_seed_path", None, "Path to the json file to be inputted to generator.")
 flags.DEFINE_integer("walk_rand_seed", None, "Seed for PRNG to be inputted (to recreate previous film)")
 flags.DEFINE_integer("walk_num", 2700, "Number of frames of walk in latent space.")
+flags.DEFINE_integer("max_jump_step", 0.03, "Maximum value for one step in jump in latent space (mode 16)")
 flags.DEFINE_integer("generation_mode", 1, "Generation mode used in testing. Please refer to README.txt")
 flags.DEFINE_string("checkpoint_name", None, "Name of the checkpoint file to load from e.g. DCGAN.model-183502")
 flags.DEFINE_string("interp_json", None, "Path to json file which contains the info needed to generate mode 10.")
