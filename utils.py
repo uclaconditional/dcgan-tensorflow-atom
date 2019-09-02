c7ff9d939fa819505121cd14d4a3bb604a0fbbc5
Hye Min Cho <3820319+hyeminchocho@users.noreply.github.com>
2019-09-02 09:14:36 -0700

    Omit import rand

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ Some codes from https://github.com/Newmu/dcgan_code
 from __future__ import division
 import math
 import json
-import random
+# import random
 import pprint
 import scipy.misc
 import numpy as np
