c85c973f21317cfdbdbfaa6065e04ec96ac02977
Hye Min Cho <3820319+hyeminchocho@users.noreply.github.com>
2019-09-12 13:37:14 -0700

    Fix mode 15 traverse latent space by number

--- main.py
+++ main.py
@@ -118,6 +118,7 @@ def main(_):
       if "base_dir" in config_json:
         base_dir = config_json["base_dir"]
 
+      pdb.set_trace()
       # Make dir with timestamp and update FLAGS.sample_dir
       json_file_name = FLAGS.gen_json.split(".")[0]
       full_gen_path = "/".join((FLAGS.sample_dir, json_file_name + "-" + FLAGS.dataset + "-" + time_stamp))
